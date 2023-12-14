937460587c06ea7559955376f281913c968486de
codethakur <kishankt2017@gmail.com>
2023-12-14 11:59:31 +0530

    Encapsulation |-setter |-getter

--- oop.cpp
+++ oop.cpp
@@ -2,10 +2,34 @@
 using namespace std;
 
 class Employe {
-public:
+private:
 	string Name;
 	string Company;
 	int Age;
+public:
+	
+	void setName(string name){ //setter
+		Name = name;
+	}
+	string getName() { //getter
+		return Name;
+	}
+
+	void setComapny(string company) { //setter
+		Company = company;
+	}
+	string getComapny() { //getter
+		return Company;
+	}
+
+	void setAge(int age) {  //setter
+		if(age>=18) 
+		Age = age;
+	}
+	int getAge() {  //getter
+		return Age;
+	}
+	
 
 	void introduceYourSelf() {
 		std::cout << "Name: " << Name << std::endl;
@@ -24,6 +48,11 @@ int main() {
 
 	Employe employe2 = Employe("Robert", "LinkedIn", 35);//constructor invoked;
 	employe2.introduceYourSelf();
+
+	/*employe2.setAge(40);
+	std::cout << employe2.getName() << " is now " << employe2.getAge() << " years old" << endl;*/
+	employe2.setAge(15);
+	std::cout << employe2.getName() << " is now " << employe2.getAge() << " years old" << endl;
 
 	system("pause>0");
 }
