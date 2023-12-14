a2ae87ad4fafbcb8fb4a794d9b85f292ac6c660d
codethakur <kishankt2017@gmail.com>
2023-12-14 12:22:11 +0530

    Abstract class

--- oop.cpp
+++ oop.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
-class Employe {
+class AbstractEmployee {
+	virtual void AskForPromotion() = 0;
+};
+
+class Employee:AbstractEmployee {
 private:
 	string Name;
 	string Company;
@@ -36,23 +40,32 @@ public:
 		std::cout << "Company: " << Company << std::endl;
 		std::cout << "Age: " << Age << std::endl;
 	}
-	Employe(string name, string company, int age) {
+	Employee(string name, string company, int age) {
 		Name = name;
 		Company = company;
 		Age = age;
 	}
+	void AskForPromotion() {
+		if (Age > 30)
+			std::cout << Name << "  got promoted." << endl;
+		else
+			std::cout<<Name<<"  Sry! No Promotion for you."<<endl;
+	}
 };
 int main() {
-	Employe employe1 = Employe("Kishan", "YT-KT", 25);//constructor invoked
-	employe1.introduceYourSelf();
+	Employee employee1 = Employee("Kishan", "YT-KT", 25);//constructor invoked
+	employee1.introduceYourSelf();
 
-	Employe employe2 = Employe("Robert", "LinkedIn", 35);//constructor invoked;
-	employe2.introduceYourSelf();
+	Employee employee2 = Employee("Robert", "LinkedIn", 35);//constructor invoked;
+	employee2.introduceYourSelf();
 
-	/*employe2.setAge(40);
-	std::cout << employe2.getName() << " is now " << employe2.getAge() << " years old" << endl;*/
-	employe2.setAge(15);
-	std::cout << employe2.getName() << " is now " << employe2.getAge() << " years old" << endl;
+	///*employe2.setAge(40);
+	//std::cout << employe2.getName() << " is now " << employe2.getAge() << " years old" << endl;*/
+	//employe2.setAge(15);
+	//std::cout << employe2.getName() << " is now " << employe2.getAge() << " years old" << endl;
+
+	employee1.AskForPromotion();
+	employee2.AskForPromotion();
 
 	system("pause>0");
 }
